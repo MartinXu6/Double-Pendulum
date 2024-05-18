8f85cb5b1251b4034e54ec419d390a693d9ae811
MartinXu <20XuPM@tonbridge-school.org>
2024-05-18 20:54:24 +0100

    remade pendulum

--- Main.py
+++ Main.py
@@ -1,81 +1,92 @@
+# import libraries
+from sympy import *
+from scipy.integrate import odeint
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import animation
-import numpy as np
-import sympy as smp
-from scipy.integrate import odeint
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import PillowWriter
 
-# Firstly defining all symbols, making calculations less messy
+# constants
+G = Symbol("G")
+M1 = Symbol("M1")
+M2 = Symbol("M2")
+L1 = Symbol("L1")
+L2 = Symbol("L2")
 
-# variables which user can adjust
-# they are time, gravitational acceleration, masses of balls, length of rods
-t, g, m1, m2, L1, L2 = smp.symbols('t g m1 m2 L1 L2')
-# the angles are a function respect to time
-the1, the2 = smp.symbols(r"\theta_1, \theta_2", cls=smp.Function)
-the1 = the1(t)
-the2 = the2(t)
+# moment of inertia
 
-# define 1st and 2nd differential of thetas in respect to time.
+I = 1 / 12 * M1 * L1 ** 2
 
-the1_d = smp.diff(the1, t)
-the2_d = smp.diff(the2, t)
-the1_dd = smp.diff(the1_d, t)
-the2_dd = smp.diff(the2_d, t)
+# the angles, which are functions of time
+T = symbols("T")
+the1 = symbols("the1", cls=Function)
+the2 = symbols("the2", cls=Function)
+the1 = the1(T)
+the2 = the2(T)
 
-# define x and y coordinates 2 masses
-x1 = L1 * smp.sin(the1)
-y1 = -L1 * smp.cos(the1)
-x2 = L1 * smp.sin(the1) + L2 * smp.sin(the2)
-y2 = -L1 * smp.cos(the1) - L2 * smp.cos(the2)
+# working out centers of mass of two rods
 
-# define energies of each mass
+x1 = L1 * sin(the1)
+y1 = -L1 * cos(the1)
+x2 = x1 + L2 * sin(the2)
+y2 = y1 - L2 * cos(the2)
 
-# Kinetic
-T1 = 1 / 2 * m1 * (smp.diff(x1, t) ** 2 + smp.diff(y1, t) ** 2)
-T2 = 1 / 2 * m2 * (smp.diff(x2, t) ** 2 + smp.diff(y2, t) ** 2)
-T = T1 + T2
-# Potential
-V1 = m1 * g * y1
-V2 = m2 * g * y2
-V = V1 + V2
+# calculating differentials
+the1d = diff(the1, T)
+the2d = diff(the2, T)
+the1dd = diff(the1d, T)
+the2dd = diff(the2d, T)
+Vx1 = diff(x1, T)
+Vx2 = diff(x2, T)
+Vy1 = diff(y1, T)
+Vy2 = diff(y2, T)
+
+# calculation of the Lagrangian
+# kinetic energy
+KE1 = 1 / 2 * M1 * (Vx1 ** 2 + Vy1 ** 2)
+KE2 = 1 / 2 * M2 * (Vx2 ** 2 + Vy2 ** 2)
+
+# rotational kinetic energy about center of mass of each rod
+RE = 1 / 2 * I * (the1d ** 2 + the2d ** 2)
+
+# GPE
+GPE = M1 * G * y1 + M2 * G * y2
+
 # Lagrangian
-L = T - V
 
-LE1 = smp.diff(L, the1) - smp.diff(smp.diff(L, the1_d), t).simplify()
-LE2 = smp.diff(L, the2) - smp.diff(smp.diff(L, the2_d), t).simplify()
+Lag = (KE1 + KE2 + RE - GPE)
 
-sols = smp.solve([LE1, LE2], (the1_dd, the2_dd),
-                 simplify=False, rational=False)
+LE1 = diff(Lag, the1) - diff(diff(Lag, the1d), T).simplify()
+LE2 = diff(Lag, the2) - diff(diff(Lag, the2d), T).simplify()
 
-dz1dt_f = smp.lambdify((t, g, m1, m2, L1, L2, the1, the2, the1_d, the2_d), sols[the1_dd])
-dz2dt_f = smp.lambdify((t, g, m1, m2, L1, L2, the1, the2, the1_d, the2_d), sols[the2_dd])
-dthe1dt_f = smp.lambdify(the1_d, the1_d)
-dthe2dt_f = smp.lambdify(the2_d, the2_d)
+sols = solve([LE1, LE2], (the1dd, the2dd),
+             simplify=False, rational=False)
+dz1dt_f = lambdify((T, G, M1, M2, L1, L2, the1, the2, the1d, the2d), sols[the1dd])
+dz2dt_f = lambdify((T, G, M1, M2, L1, L2, the1, the2, the1d, the2d), sols[the2dd])
+dthe1dt_f = lambdify(the1d, the1d)
+dthe2dt_f = lambdify(the2d, the2d)
 
 
-def dSdt(S, t, g, m1, m2, L1, L2):
+def dSdt(S, T, G, M1, M2, L1, L2):
     the1, z1, the2, z2 = S
     return [
         dthe1dt_f(z1),
-        dz1dt_f(t, g, m1, m2, L1, L2, the1, the2, z1, z2),
+        dz1dt_f(T, G, M1, M2, L1, L2, the1, the2, z1, z2),
         dthe2dt_f(z2),
-        dz2dt_f(t, g, m1, m2, L1, L2, the1, the2, z1, z2),
+        dz2dt_f(T, G, M1, M2, L1, L2, the1, the2, z1, z2),
     ]
 
 
-t = np.linspace(0, 40, 1001)
-g = 100
-m1 = 100
-m2 = 1
+T = np.linspace(0, 40, 1001)
+G = 9.81
+M1 = 2
+M2 = 1
 L1 = 2
-L2 = 3
-ans = odeint(dSdt, y0=[1, -3, -1, 5], t=t, args=(g, m1, m2, L1, L2))
+L2 = 1
+ans = odeint(dSdt, y0=[1, -3, -1, 5], t=T, args=(G, M1, M2, L1, L2))
 
 the1 = ans.T[0]
-the2 = ans.T[1]
-
-plt.plot(the1, the2)
+the2 = ans.T[2]
+plt.plot(T, the2)
 plt.show()
 
 
@@ -86,7 +97,7 @@ def get_x1y1x2y2(t, the1, the2, L1, L2):
             -L1 * np.cos(the1) - L2 * np.cos(the2))
 
 
-x1, y1, x2, y2 = get_x1y1x2y2(t, ans.T[0], ans.T[2], L1, L2)
+x1, y1, x2, y2 = get_x1y1x2y2(T, ans.T[0], ans.T[2], L1, L2)
 
 
 def animate(i):
@@ -97,7 +108,7 @@ fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.set_facecolor('k')
 ax.get_xaxis().set_ticks([])  # enable this to hide x axis ticks
 ax.get_yaxis().set_ticks([])  # enable this to hide y axis ticks
-ln1, = plt.plot([], [], 'o', lw=3, markersize=8)
+ln1, = plt.plot([], [], 'g-', lw=3, markersize=8)
 ax.set_ylim(-4, 4)
 ax.set_xlim(-4, 4)
 ani = animation.FuncAnimation(fig, animate, frames=1000, interval=50)
