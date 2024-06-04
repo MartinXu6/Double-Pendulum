a05d6a7db421246a471de34b33f919139fba7e3f
Martin <116567565+MartinXu6@users.noreply.github.com>
2024-06-04 20:09:59 +0100

    refined algorithm

--- Main.py
+++ Main.py
@@ -1,104 +1,85 @@
-
-# import libraries
-from sympy import *
-from scipy.integrate import odeint
 import numpy as np
+from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from matplotlib import animation
 
-# constants
-G = Symbol("G")
-M1 = Symbol("M1")
-M2 = Symbol("M2")
-L1 = Symbol("L1")
-L2 = Symbol("L2")
-
-# moment of inertia
-
-I = 1 / 12 * M1 * L1 ** 2
-
-# the angles, which are functions of time
-T = symbols("T")
-the1 = symbols("the1", cls=Function)
-the2 = symbols("the2", cls=Function)
-the1 = the1(T)
-the2 = the2(T)
-
-# working out centers of mass of two rods
-
-x1 = L1 * sin(the1)
-y1 = -L1 * cos(the1)
-x2 = x1 + L2 * sin(the2)
-y2 = y1 - L2 * cos(the2)
-
-# calculating differentials
-the1d = diff(the1, T)
-the2d = diff(the2, T)
-the1dd = diff(the1d, T)
-the2dd = diff(the2d, T)
-Vx1 = diff(x1, T)
-Vx2 = diff(x2, T)
-Vy1 = diff(y1, T)
-Vy2 = diff(y2, T)
-
-# calculation of the Lagrangian
-# kinetic energy
-KE1 = 1 / 2 * M1 * (Vx1 ** 2 + Vy1 ** 2)
-KE2 = 1 / 2 * M2 * (Vx2 ** 2 + Vy2 ** 2)
-
-# rotational kinetic energy about center of mass of each rod
-RE = 1 / 2 * I * (the1d ** 2 + the2d ** 2)
-
-# GPE
-GPE = M1 * G * y1 + M2 * G * y2
-
-# Lagrangian
-
-Lag = (KE1 + KE2 + RE - GPE)
-
-LE1 = diff(Lag, the1) - diff(diff(Lag, the1d), T).simplify()
-LE2 = diff(Lag, the2) - diff(diff(Lag, the2d), T).simplify()
-
-sols = solve([LE1, LE2], (the1dd, the2dd),
-             simplify=False, rational=False)
-dz1dt_f = lambdify((T, G, M1, M2, L1, L2, the1, the2, the1d, the2d), sols[the1dd])
-dz2dt_f = lambdify((T, G, M1, M2, L1, L2, the1, the2, the1d, the2d), sols[the2dd])
-dthe1dt_f = lambdify(the1d, the1d)
-dthe2dt_f = lambdify(the2d, the2d)
+# Parameters
+l1 = 3  # length of pendulum 1 (m)
+l2 = 5  # length of pendulum 2 (m)
+m1 = 2  # mass of pendulum 1 (kg)
+m2 = 2  # mass of pendulum 2 (kg)
+g = 9.81  # acceleration due to gravity (m/s^2)
 
 
-def dSdt(S, T, G, M1, M2, L1, L2):
-    the1, z1, the2, z2 = S
-    return [
-        dthe1dt_f(z1),
-        dz1dt_f(T, G, M1, M2, L1, L2, the1, the2, z1, z2),
-        dthe2dt_f(z2),
-        dz2dt_f(T, G, M1, M2, L1, L2, the1, the2, z1, z2),
+initial_conditions = [
+    [np.pi / 2, 0, np.pi, 0],
+]
+
+# Time array
+t = np.linspace(0, 40, 1000)
+
+
+def equations(y, t, l1, l2, m1, m2, g):
+    theta1, omega1, theta2, omega2 = y
+
+    delta_theta = theta2 - theta1
+
+    den1 = (m1 + m2) * l1 - m2 * l1 * np.cos(delta_theta) * np.cos(delta_theta)
+    den2 = (l2 / l1) * den1
+
+    dydt = [
+        omega1,
+        ((m2 * l1 * omega1 * omega1 * np.sin(delta_theta) * np.cos(delta_theta) +
+          m2 * g * np.sin(theta2) * np.cos(delta_theta) +
+          m2 * l2 * omega2 * omega2 * np.sin(delta_theta) -
+          (m1 + m2) * g * np.sin(theta1)) / den1),
+        omega2,
+        ((-m2 * l2 * omega2 * omega2 * np.sin(delta_theta) * np.cos(delta_theta) +
+          (m1 + m2) * g * np.sin(theta1) * np.cos(delta_theta) -
+          (m1 + m2) * l1 * omega1 * omega1 * np.sin(delta_theta) -
+          (m1 + m2) * g * np.sin(theta2)) / den2)
     ]
+    return dydt
 
 
-T = np.linspace(0, 40, 1001)
-G = 9.81
-M1 = 2
-M2 = 2
-L1 = 2
-L2 = 2
-ans = odeint(dSdt, y0=[1, -3, -1, 5], t=T, args=(G, M1, M2, L1, L2))
+# Perform numerical integration for each set of initial conditions
+results = []
+for ic in initial_conditions:
+    sol = odeint(equations, ic, t, args=(l1, l2, m1, m2, g))
+    results.append(sol)
 
-the1 = ans.T[0]
-the2 = ans.T[2]
-plt.plot(T, the2)
+# Plot the results
+fig, ax = plt.subplots(2, 1, figsize=(10, 8))
+
+for i, sol in enumerate(results):
+    theta1 = sol[:, 0]
+    theta2 = sol[:, 2]
+
+    ax[0].plot(t, theta1, label=f'Set {i + 1}')
+    ax[1].plot(t, theta2, label=f'Set {i + 1}')
+
+ax[0].set_xlabel('Time [s]')
+ax[0].set_ylabel('Theta1 [rad]')
+ax[0].legend()
+ax[0].set_title('Theta1 vs Time')
+
+ax[1].set_xlabel('Time [s]')
+ax[1].set_ylabel('Theta2 [rad]')
+ax[1].legend()
+ax[1].set_title('Theta2 vs Time')
+
+plt.tight_layout()
 plt.show()
 
 
-def get_x1y1x2y2(t, the1, the2, L1, L2):
+def get_x1y1x2y2(the1, the2, L1, L2):
     return (L1 * np.sin(the1),
             -L1 * np.cos(the1),
             L1 * np.sin(the1) + L2 * np.sin(the2),
             -L1 * np.cos(the1) - L2 * np.cos(the2))
 
 
-x1, y1, x2, y2 = get_x1y1x2y2(T, ans.T[0], ans.T[2], L1, L2)
+x1, y1, x2, y2 = get_x1y1x2y2(theta1, theta2, l1, l2)
 
 
 def animate(i):
@@ -109,8 +90,8 @@ fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.set_facecolor('k')
 ax.get_xaxis().set_ticks([])  # enable this to hide x axis ticks
 ax.get_yaxis().set_ticks([])  # enable this to hide y axis ticks
-ln1, = plt.plot([], [], 'g-', lw=3, markersize=8)
-ax.set_ylim(-4, 4)
-ax.set_xlim(-4, 4)
+ln1, = plt.plot([], [], 'go-', lw=3, markersize=8)
+ax.set_ylim(-10, 10)
+ax.set_xlim(-10, 10)
 ani = animation.FuncAnimation(fig, animate, frames=1000, interval=50)
 ani.save('pen.gif', writer='pillow', fps=25)
